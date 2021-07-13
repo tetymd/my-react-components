89bf5e0c03ca33c4dcba837e6e6db594257fc886
Tetsuya Yamada <31818868+tetymd@users.noreply.github.com>
2021-07-13 23:11:46 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./styles.css";
+import Home from "./domain/Home";
+
+export default function App() {
+  return <Home></Home>;
+}
---
+++ src/domain/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Box, Button } from "@material-ui/core";
+
+export const Home = () => {
+  return (
+    <Box>
+      <Button>Touch Me!</Button>
+    </Box>
+  );
+};
